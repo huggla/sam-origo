02f5d2b5fa5f9722ef034ac2b60087374e870c24
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-02-07 13:21:43 +0100

    Create 100.php

---
+++ finalfs/start/stage3/100.php
@@ -0,0 +1,22 @@
+# Set in stage2:
+# ---------------------------------------------------------
+# set -ef +am
+# isFirstRun
+# VAR_*
+# All functions in /start/functions
+
+if [ "$isFirstRun" == "true" ]
+then
+   if [ ! -s "/etc/php7/php-fpm.d/www.conf" ]
+   then
+      createWwwConf
+   fi
+   if [ ! -s "/etc/php7/conf.d/50-setting.ini" ]
+   then
+      setPhpIni
+   fi
+   if [ ! -s "/etc/ldap/ldap.conf" ]
+   then
+      createLdapConf
+   fi
+fi
