4b8f112422ad5433ab0c90d0113e2d5dadd6c230
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-02-08 12:26:52 +0100

    Create array_column_search.php

---
+++ finalfs/origo/adm/functions/array_column_search.php
@@ -0,0 +1,22 @@
+<?php
+function array_column_search($search, $column, $array, $return=false)
+{
+	$columnValues = array_column($array, $column);
+	$key = array_search($search, $columnValues);
+	if ( $key !== false )
+	{
+		if (!$return)
+		{
+			return $array[$key];
+		}
+		else
+		{
+			return $array[$key][$return];
+		}
+	}
+	else
+	{
+		return array();
+	}
+}
+?>
