73291e75c4d7dbf97bded7a5cb889a996d5dd6da
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-02-09 11:46:23 +0100

    Update dbh.php

--- finalfs/origo/adm/functions/dbh.php
+++ finalfs/origo/adm/functions/dbh.php
@@ -1,7 +1,7 @@
 <?php
   function dbh()
   {
-	  $dbh = pg_connect("host=config-db port=5432 dbname=origo-config user=postgres password=postgres");
+	  $dbh = pg_connect("host=localhost port=5432 dbname=origo user=postgres password=postgres");
 	  if (!$dbh)
 	  {
 		  echo '{"save_status":"Error in connection"}';
