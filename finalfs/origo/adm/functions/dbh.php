e4cb21a3cb8fb45f6d59800fb90052b1b66c6590
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-02-10 15:40:09 +0100

    Update dbh.php

--- finalfs/origo/adm/functions/dbh.php
+++ finalfs/origo/adm/functions/dbh.php
@@ -1,15 +1,17 @@
 <?php
-  function dbh()
-  {
-	  $dbh = pg_connect("host=localhost port=5432 dbname=origo user=postgres password=postgres");
-	  if (!$dbh)
-	  {
-		  echo '{"save_status":"Error in connection"}';
-		  die();
-	  }
-    else
-    {
-      return $dbh;
-    }
-  }
+
+	function dbh($connection_string)
+	{
+		$dbh = pg_connect($connection_string);
+		if (!$dbh)
+		{
+			echo '{"save_status":"Error in connection"}';
+			die();
+		}
+		else
+		{
+			return $dbh;
+		}
+	}
+
 ?>
