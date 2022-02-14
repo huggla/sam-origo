b9b6961328a328b240af12c26a0c184cae8c7719
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-02-14 15:11:37 +0100

    Create addSourcesToJson.php

---
+++ finalfs/origo/adm/functions/writeConfig/addSourcesToJson.php
@@ -0,0 +1,69 @@
+<?php
+
+	function addSourcesToJson()
+	{
+		GLOBAL $json, $mapSources, $sources, $services;
+		$json = $json.'"source": { ';
+		if (!is_array($mapSources))
+		{
+			$mapSources = pgArrayToPhp($mapSources);
+		}
+		$mapSources = array_unique($mapSources);
+		$firstSource = true;
+		foreach ($mapSources as $sourceId)
+		{
+			if ($firstSource)
+			{
+				$firstSource = false;
+			}
+			else
+			{
+				$json = $json.', ';
+			}
+			$source = array_column_search(trim(explode('@', $sourceId, 2)[0]), 'source_id', $sources);
+			$url = array_column_search($source['service'], 'service_id', $services, 'base_url');
+			$sourceProject = trim(explode('#', $source['source_id'], 2)[0]);
+			if (strpos($sourceId, '@wfs') !== false)
+			{
+				$wfsSource = true;
+			}
+			else
+			{
+				$wfsSource = false;
+			}
+			$url = rtrim($url, '/').'/'.$sourceProject;
+			$sourceColumns = array_keys($source);
+			$queryColumns = array();
+			if (!$wfsSource)
+			{
+				foreach ($sourceColumns as $column)
+				{
+					if ($column != 'source_id' && $column != 'abstract' && $column != 'base_url' && $column != 'service' && $column != 'project' && !empty($source[$column]))
+					{
+						$queryColumns[] = $column;
+					}
+				}
+				foreach ($queryColumns as $query)
+				{
+					if (strpos($url, '?') === false)
+					{
+						$url = $url.'?';
+					}
+					else
+					{
+						$url = $url.'&';
+					}
+					$url = $url.$query.'='.pgBoolToText($source[$query]);
+				}
+			}
+			$json = $json.'"'.$sourceId.'": { "url": "'.$url.'"';
+			if ($wfsSource)
+			{
+				$json = $json.', "workspace": "qgs"';
+			}
+			$json = $json.'}';
+		}
+		$json = $json.' }';
+	}
+
+?>
