4012b1c731eedc9b85d1a8850c8b75d301859e26
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-02-14 15:10:21 +0100

    Create addGroupsToJson.php

---
+++ finalfs/origo/adm/functions/writeConfig/addGroupsToJson.php
@@ -0,0 +1,40 @@
+<?php
+
+	function addGroupsToJson($mapGroups)
+	{
+		GLOBAL $groups, $json, $mapLayers;
+		if (!empty($mapGroups))
+		{
+			$mapGroups = pgArrayToPhp($mapGroups);
+			$json = $json. '"groups": [ ';
+			$firstGroup = true;
+			foreach ($mapGroups as $group)
+			{
+				if ($firstGroup)
+				{
+					$firstGroup = false;
+				}
+				else
+				{
+					$json = $json.', ';
+				}
+				$group = array_column_search($group, 'group_id', $groups);
+				$groupName = trim(explode('#', $group['group_id'], 2)[0]);
+				$mapLayers = array_merge($mapLayers, array($groupName => pgArrayToPhp($group['layers'])));
+				$json = $json.'{ "name": "'.$groupName.'", "title": "'.$group['title'].'", "expanded": '.pgBoolToText($group['expanded']);
+				if (!empty($group['abstract']))
+				{
+					$json = $json.', "abstract": "'.$group['abstract'].'"';
+				}
+				if (!empty($group['groups']))
+				{
+					$json = $json.', ';
+					addGroupsToJson($group['groups']);
+				}
+				$json = $json.' }';
+			}
+			$json = $json.' ]';
+		}
+	}
+
+?>
