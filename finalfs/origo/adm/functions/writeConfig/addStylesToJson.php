3247dacb24aa08544ce209f0aa03491f7a7a22ca
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-02-14 15:12:17 +0100

    Create addStylesToJson.php

---
+++ finalfs/origo/adm/functions/writeConfig/addStylesToJson.php
@@ -0,0 +1,56 @@
+<?php
+
+	function addStylesToJson()
+	{
+		GLOBAL $mapStyles, $styles, $json, $mapSources;
+		$json = $json.'"styles": {';
+		$firstStyle = true;
+		foreach ($mapStyles as $style)
+		{
+			if ($firstStyle)
+			{
+				$firstStyle = false;
+			}
+			else
+			{
+				$json = $json.', ';
+			}
+			$json = $json.'"'.$style['layer_id'].'": ';
+			if (!empty($style['style_config']) && $style['style_config'] != '[]' && $style['style_config'] != '{}' && $style['style_config'] != '""' && $style['style_config'] != 'null')
+			{
+				$json = $json.$style['style_config'];
+			}
+			else
+			{
+				$json = $json.'[ [ {';
+				if (!empty($style['label']))
+				{
+					$json = $json.'"label": "'.$style['label'].'", ';
+				}
+				if (!empty($style['icon']))
+				{
+					if (empty($style['icon_extended']) && $style['type'] == 'WMS')
+					{
+						$json = $json.'"image": { "src": "'.$style['icon'].'" }';
+					}
+					else
+					{
+						$json = $json.'"icon": { "src": "'.$style['icon'].'" }';
+					}
+				}
+				if (!empty($style['style_filter']))
+				{
+					$json = $json.', "filter": "'.$style['style_filter'].'"';
+				}
+				$json = $json.' }';
+				if (!empty($style['icon_extended']))
+				{
+					$json = $json.',{ "icon": { "src": "'.$style['icon_extended'].'" }, "extendedLegend": true }';
+				}
+ 				$json = $json.']]';
+			}
+		}
+		$json = $json.' }';
+	}
+
+?>
