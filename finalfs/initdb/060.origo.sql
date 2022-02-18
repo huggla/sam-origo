29b37fb95627cb2a26108ee652338b4911e68cc8
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-02-18 12:22:38 +0100

    Update 060.origo.sql

--- finalfs/initdb/060.origo.sql
+++ finalfs/initdb/060.origo.sql
@@ -66,6 +66,7 @@ CREATE TABLE map_configs.layers
     gutter integer,
     tiled boolean DEFAULT true,
     opacity numeric(3,2) NOT NULL DEFAULT 1,
+    featureinfolayer character varying COLLATE pg_catalog."default",
     info character varying COLLATE pg_catalog."default",
     CONSTRAINT layers_pkey PRIMARY KEY (layer_id)
 );
