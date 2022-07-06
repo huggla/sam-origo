122d68344994715da2a2e89940578f1c59ab06a5
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-07-06 15:20:44 +0200

    Update addGroupsToJson.php

--- finalfs/origo/adm/functions/writeConfig/addGroupsToJson.php
+++ finalfs/origo/adm/functions/writeConfig/addGroupsToJson.php
@@ -10,27 +10,34 @@
 			$firstGroup = true;
 			foreach ($mapGroups as $group)
 			{
-				if ($firstGroup)
-				{
-					$firstGroup = false;
-				}
-				else
-				{
-					$json = $json.', ';
-				}
 				$group = array_column_search($group, 'group_id', $groups);
 				$groupName = trim(explode('#', $group['group_id'], 2)[0]);
 				$mapLayers = array_merge($mapLayers, array($groupName => pgArrayToPhp($group['layers'])));
-				$json = $json.'{ "name": "'.$groupName.'", "title": "'.$group['title'].'", "expanded": '.pgBoolToText($group['expanded']);
-				if (!empty($group['abstract']))
+				if ($groupName !== 'none')
 				{
-					$json = $json.', "abstract": "'.$group['abstract'].'"';
+					if ($firstGroup)
+					{
+						$firstGroup = false;
+					}
+					else
+					{
+						$json = $json.', ';
+					}
+					$json = $json.'{ "name": "'.$groupName.'", "title": "'.$group['title'].'"';
+					if ($group['expanded'] == 't')
+					{
+						$json = $json.', "expanded": true';
+					}
+					if (!empty($group['abstract']))
+					{
+						$json = $json.', "abstract": "'.$group['abstract'].'"';
+					}
+					if (!empty(trim($group['groups'], '{}')))
+					{
+						addGroupsToJson($group['groups']);
+					}
+					$json = $json.' }';
 				}
-				if (!empty(trim($group['groups'], '{}')))
-				{
-					addGroupsToJson($group['groups']);
-				}
-				$json = $json.' }';
 			}
 			$json = $json.' ]';
 		}
