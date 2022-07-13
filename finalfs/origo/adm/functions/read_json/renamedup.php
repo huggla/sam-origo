7362cd1d87f3bb639b30b416a30746ed0a610ed3
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-07-13 10:31:08 +0200

    Update renamedup.php

--- finalfs/origo/adm/functions/read_json/renamedup.php
+++ finalfs/origo/adm/functions/read_json/renamedup.php
@@ -1,1 +1,26 @@
+<?php
 
+function renamedup($name, $count=0)
+{
+	GLOBAL $uniqueLayers;
+	if ($count > 0)
+	{
+		$newname="$name#$count";
+	}
+	else
+	{
+		$newname="$name#";
+	}
+	if (in_array($newname, $uniqueLayers))
+	{
+		$count++;
+		return renamedup($name, $count);
+	}
+	else
+	{
+		$uniqueLayers[]=$newname;
+		return $newname;
+	}
+}
+
+?>
