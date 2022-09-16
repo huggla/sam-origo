c458a09f41d0aaf8626ecc17e49d48982d7b75ef
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-09-16 13:37:46 +0200

    Update manage.php

--- finalfs/origo/adm/manage.php
+++ finalfs/origo/adm/manage.php
@@ -936,7 +936,7 @@
 					echo '</form>';
 				}
 			}
-			echo '<div style="white-space:nowrap;margin-bottom:-1vw">';
+			echo '<div style="white-space:nowrap;margin-bottom:-2vw">';
 			echo   "<form method='post' onsubmit='confirmStr=\"$confirmStr\"; return confirm(confirmStr);' style='line-height:2'>";
 			if (isset($mapId))
 			{
