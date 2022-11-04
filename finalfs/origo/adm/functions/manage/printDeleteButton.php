3a818da91184f1a515822d0fa1f52cfc63027f64
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-11-04 12:51:19 +0100

    Create printDeleteButton.php

---
+++ finalfs/origo/adm/functions/manage/printDeleteButton.php
@@ -0,0 +1,14 @@
+<?php
+	function printDeleteButton($targetId, $deleteConfirmStr, $deleteButtonClass)
+	{
+		$target=$GLOBALS['target'];
+		echo <<<HERE
+			<div class="deleteButtonDiv">
+				<form method='post' onsubmit='confirmStr="{$deleteConfirmStr}"; return confirm(confirmStr);' style='line-height:2'>
+					<input type="hidden" name="{$target}IdDel" value="{$mapId}">
+					<button class='{$deleteButtonClass}' type='submit' name='{$target}Button' value='delete'>Radera</button>
+				</form>
+			</div>
+		HERE;
+	}
+?>
