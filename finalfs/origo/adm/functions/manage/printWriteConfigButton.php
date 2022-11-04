82885ec8677950976f3366b529eeb4463cbcd273
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-11-04 12:53:23 +0100

    Create printWriteConfigButton.php

---
+++ finalfs/origo/adm/functions/manage/printWriteConfigButton.php
@@ -0,0 +1,14 @@
+<?php
+	function printWriteConfigButton()
+	{
+		GLOBAL $mapId;
+		$confirmStr="Är du säker att du vill skriva över den befintliga konfigurationen för $mapId?";
+		echo <<<HERE
+			<form onsubmit='confirmStr="{$confirmStr}"; return confirm(confirmStr);' action="writeConfig.php" method="get" target="hiddenFrame">
+				<button class="updateButton" type="submit" name="map" value="{$mapId}">
+					Skriv kartkonfiguration
+				</button>
+			</form>
+		HERE;
+	}
+?>
