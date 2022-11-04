44b8cdcd1ea2dac5e67cfb531558e8d866a1ba29
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-11-04 12:55:23 +0100

    Update infoButton.php

--- finalfs/origo/adm/functions/manage/infoButton.php
+++ finalfs/origo/adm/functions/manage/infoButton.php
@@ -2,13 +2,11 @@
 
 	function infoButton($type)
 	{
-		GLOBAL $mapId, $groupId, $controlId, $layerId, $sourceId, $serviceId, $footerId, $tilegridId, $proj4defId;
-		$evalStr="\$id=\$".$type."Id;";
-		eval($evalStr);
+		$idVarStr=$type.'Id';
 		echo   '<form></form>';
 		echo   '<form action="info.php" method="get" target="topFrame">';
 		echo     '<input type="hidden" name="type" value="'.$type.'">';
-		echo     "<button class=\"updateButton\" onclick=\"toggleTopFrame('info');\" type=\"submit\" name=\"id\" value=\"$id\">";
+		echo     "<button class=\"updateButton\" onclick=\"toggleTopFrame('info');\" type=\"submit\" name=\"id\" value=\"".$GLOBALS[$idVarStr].'">';
 		echo       'Info';
 		echo     '</button>';
 		echo   '</form>';
