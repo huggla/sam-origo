c7835231e97e6ef381ebf5cb43146597f38895e2
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-11-04 12:23:13 +0100

    Create printRemoveOperation.php

---
+++ finalfs/origo/adm/functions/manage/printRemoveOperation.php
@@ -0,0 +1,20 @@
+<?php
+	function printRemoveOperation($tablename, $targetId, $buttontext)
+	{
+			GLOBAL $target;
+			$tabletype=rtrim($tablename, 's');
+			$mapParents=findParents($tabletype, $target, $targetId);
+			if (!empty($mapParents))
+			{
+				$str=ucfirst($tabletype);
+				echo '<form class="addForm" method="post">';
+				echo '<select class="headSelect" name="from'.$str.'Id">';
+				printSelectOptions($mapParents);
+				echo '</select>';
+				echo '<input type="hidden" name="'.$target.'Id" value="'.$targetId.'">';
+				echo $hiddenInputs;
+				echo '<button type="submit" name="'.$target.'Button" value="operation">'.$buttontext.'</button>';
+				echo '</form>';
+			}
+	}
+?>
