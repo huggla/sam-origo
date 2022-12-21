cd3d76dfa346602d183df99ddad3b3d32f957e14
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-12-21 11:40:33 +0100

    Update printProj4defForm.php

--- finalfs/origo/adm/functions/manage/printProj4defForm.php
+++ finalfs/origo/adm/functions/manage/printProj4defForm.php
@@ -1,26 +1,20 @@
 <?php
-	function printProj4defForm($targetId)
+	function printProj4defForm($proj4def, $inheritPosts)
 	{
-		GLOBAL $hiddenInputs, $target;
-		echo <<<HERE
-		<div>
-			<div style="float:left;">
-				<form method="post" style="line-height:2">
-		HERE;
-		printTextarea($targetId, 'code', 'textareaMedium', 'Kod:');
-		printTextarea($targetId, 'projection', 'textareaLarge', 'Projektion:');
-		printTextarea($targetId, 'alias', 'textareaMedium');
+		echo '<div><div style="float:left;"><form method="post" style="line-height:2">';
+		printTextarea($proj4def, 'code', 'textareaMedium', 'Kod:');
+		printTextarea($proj4def, 'projection', 'textareaLarge', 'Projektion:');
+		printTextarea($proj4def, 'alias', 'textareaMedium', 'Alias:');
 		echo '</br>';
-		printTextarea($targetId, 'info', 'textareaLarge');
-		echo $hiddenInputs;
-		echo 			'<div class="buttonDiv">';
-		printUpdateButton();
-                printInfoButton($targetId);
-		echo 			'</div>';
-		echo 		'</form>';
-		echo 	'</div>';
-		$deleteConfirmStr="Är du säker att du vill radera proj4def $targetId? Referenser till aktuell proj4def hanteras separat.";
-		printDeleteButton($targetId, $deleteConfirmStr, 'deleteButton2');
+		printTextarea($proj4def, 'info', 'textareaLarge', 'Info:');
+		printHiddenInputs($inheritPosts);
+		echo '<div class="buttonDiv">';
+		printUpdateButton('proj4def');
+		$proj4def['proj4def']=$proj4def['proj4def']['code'];
+		printInfoButton($proj4def);
+		echo '</div></form></div>';
+		$deleteConfirmStr="Är du säker att du vill radera proj4def ".$proj4def['proj4def']."? Referenser till aktuell proj4def hanteras separat.";
+		printDeleteButton($proj4def, $deleteConfirmStr, 'deleteButton2');
 		echo '</div>';
 	}
 ?>
