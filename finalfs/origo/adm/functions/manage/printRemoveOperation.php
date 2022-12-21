b5315178fa6d522e508c01776e51cf0a5da10448
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-12-21 11:42:13 +0100

    Update printRemoveOperation.php

--- finalfs/origo/adm/functions/manage/printRemoveOperation.php
+++ finalfs/origo/adm/functions/manage/printRemoveOperation.php
@@ -1,16 +1,16 @@
 <?php
 	function printRemoveOperation($targetToRemove, $tableToRemoveFrom, $buttontext, $inheritPosts)
 	{
-			$parents=findParents($tableToRemoveFrom, $targetToRemove);
-			if (!empty($parents))
-			{
-				echo '<form class="addForm" method="post">';
-				echo '<select class="headSelect" name="from'.ucfirst(rtrim(key($tableToRemoveFrom), 's')).'Id">';
-				printSelectOptions(array_merge(array(""), $parents));
-				echo '</select>';
-				printHiddenInputs($inheritPosts);
-				echo '<button type="submit" name="'.key($targetToRemove).'Button" value="operation">'.$buttontext.'</button>';
-				echo '</form>';
-			}
+		$parents=findParents($tableToRemoveFrom, $targetToRemove);
+		if (!empty($parents))
+		{
+			echo '<form class="addForm" method="post">';
+			echo '<select class="headSelect" name="from'.ucfirst(rtrim(key($tableToRemoveFrom), 's')).'Id">';
+			printSelectOptions(array_merge(array(""), $parents));
+			echo '</select>';
+			printHiddenInputs($inheritPosts);
+			echo '<button type="submit" name="'.key($targetToRemove).'Button" value="operation">'.$buttontext.'</button>';
+			echo '</form>';
+		}
 	}
 ?>
