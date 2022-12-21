edd361660359d6c8f6a06d7c784c787ae6a7b0cb
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-12-21 11:30:02 +0100

    Update printChildSelect.php

--- finalfs/origo/adm/functions/manage/printChildSelect.php
+++ finalfs/origo/adm/functions/manage/printChildSelect.php
@@ -1,56 +1,76 @@
 <?php
-	function printChildSelect($targetId, $column, $header, $selectedValue=null)
+	function printChildSelect($target, $column, &$thClass, $heading, $inheritPosts, $groupLevel=1, $selectedValue=null)
 	{
-
-		GLOBAL $mapId, $groupId, $groupIds, $groupLevels, $thClass;
-		$target=$GLOBALS['target'];
-		$targetTable=$target.'s';
-		$targetTableArr=$GLOBALS[$targetTable];
-		$targetArr=array_column_search($targetId, idColumnOfTarget($target), $targetTableArr);
-		$selected=$targetArr[$column];
-		$type=rtrim($column, 's');
-		if ($column == 'groups')
+		$targetColumnValue=trim(current($target)[$column], '{}');
+		if (!empty($targetColumnValue))
 		{
-		$columnId=$GLOBALS['groupIds'][$GLOBALS['level']];
-		}
-		else
-		{
-		$columnId=$GLOBALS[$type.'Id'];
-		}
-		if (!empty(trim($targetArr[$column], '{}')))
-		{
-			if (!isset($columnId))
+			$groupIdsArray=array();
+			$columnType=rtrim($column, 's');
+			$sName=$columnType.'Id';
+			$columnArr=pgArrayToPhp('{'.$targetColumnValue.'}');
+			if (key($target) == 'group' && ($column == 'groups' || $column == 'layers'))
 			{
-				$edith3Class='h3Lightgray';
+				if (isset($inheritPosts['groupIds']))
+				{
+					$groupIdsArray=explode(',', $inheritPosts['groupIds']);
+				}
+				if ($column == 'groups')
+				{
+					$sName='groupIds';
+					if (!isset($selectedValue) && isset($groupIdsArray[$groupLevel]))
+					{
+						$selectedValue=$groupIdsArray[$groupLevel];
+					}
+				}
+				$groupLevels=count($groupIdsArray);
+				if ($groupLevels > 1)
+				{
+					$groupIdsArray=array_slice($groupIdsArray, 0, $groupLevel);
+				}
+				if ($column == 'groups' && !empty($groupIdsArray))
+				{
+					$columnArr=array_map(function($val) use ($groupIdsArray) { return implode(',', $groupIdsArray).','.$val; } , $columnArr);
+				}
 			}
-			else
+			if (!isset($selectedValue) && isset($inheritPosts[$columnType.'Id']))
+			{
+				$selectedValue=$inheritPosts[$columnType.'Id'];
+			}
+			$options=array_merge(array(""), $columnArr);
+			if (isset($selectedValue) && in_array($selectedValue, $options))
 			{
 				$edith3Class='h3Black';
 			}
+			else
+			{
+				$edith3Class='h3Lightgray';
+			}
+			$targetId=current($target)[pkColumnOfTable(key($target).'s')];
+			$ucColumn=ucfirst($column);
+			$hiddenInputs=array();
+			if (isset($inheritPosts['mapId']))
+			{
+				$hiddenInputs['mapId']=$inheritPosts['mapId'];
+			}
+			if ($column == 'layers')
+			{
+				if (isset($inheritPosts['groupIds']))
+				{
+					$hiddenInputs['groupIds']=implode(',', $groupIdsArray);
+				}
+			}
 			echo <<<HERE
 				<th class="{$thClass}">
-					<h3 class="{$edith3Class}">{$header}</h3>
+					<h3 class="{$edith3Class}">{$heading}</h3>
 					<div style="display:flex">
 						<form class="headForm" method="post">
+							<select onchange='this.form.submit();' class='headSelect' id='{$targetId}{$ucColumn}' name='{$sName}'>
 			HERE;
-			printColumnSelect($targetId, $column, $selectedValue);
-			if (isset($mapId))
-			{
-				echo '<input type="hidden" name="mapId" value="'.$mapId.'">';
-			}
-			if (isset($groupId))
-			{
-				echo '<input type="hidden" name="groupId" value="'.$groupId.'">';
-			}
-
-			if ($column != 'groups' && !empty($groupIds))
-			{
-				echo '<input type="hidden" name="groupIds" value="'.implode(',', $GLOBALS['groupIds']).'">';
-				echo '<input type="hidden" name="groupLevels" value="'.$groupLevels.'">';
-			}
-
+			printSelectOptions($options, $selectedValue);
+			echo           "</select>&nbsp;";
+			printHiddenInputs($hiddenInputs);
 			echo <<<HERE
-							<button type="submit" class="headButton" name="{$type}Button" value="get">
+							<button type="submit" class="headButton" name="{$columnType}Button" value="get">
 								Hämta
 							</button>
 						</form>
