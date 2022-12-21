caf0dee618ba4d178a94cc30bc700c8e8ecf100a
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-12-21 11:37:55 +0100

    Update printInfoButton.php

--- finalfs/origo/adm/functions/manage/printInfoButton.php
+++ finalfs/origo/adm/functions/manage/printInfoButton.php
@@ -1,12 +1,13 @@
 <?php
-	function printInfoButton($targetId)
+	function printInfoButton($target)
 	{
-		$target=$GLOBALS['target'];
+		$type=key($target);
+		$id=current($target);
 		echo <<<HERE
 			<form></form>
 			<form action="info.php" method="get" target="topFrame">
-				<input type="hidden" name="type" value="{$target}">
-				<button class="updateButton" onclick="toggleTopFrame('info');" type="submit" name="id" value="{$targetId}">
+				<input type="hidden" name="type" value="{$type}">
+				<button class="updateButton" onclick="toggleTopFrame('info');" type="submit" name="id" value="{$id}">
 					Info
 				</button>
 			</form>
