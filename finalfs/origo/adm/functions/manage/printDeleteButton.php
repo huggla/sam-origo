22959b882e0ce0d1eae3107957625bac41f1f0e6
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-12-21 11:32:54 +0100

    Update printDeleteButton.php

--- finalfs/origo/adm/functions/manage/printDeleteButton.php
+++ finalfs/origo/adm/functions/manage/printDeleteButton.php
@@ -1,12 +1,13 @@
 <?php
-	function printDeleteButton($targetId, $deleteConfirmStr, $deleteButtonClass)
+	function printDeleteButton($target, $deleteConfirmStr, $deleteButtonClass)
 	{
-		$target=$GLOBALS['target'];
+		$targetType=key($target);
+		$targetId=current($target);
 		echo <<<HERE
 			<div class="deleteButtonDiv">
 				<form method='post' onsubmit='confirmStr="{$deleteConfirmStr}"; return confirm(confirmStr);' style='line-height:2'>
-					<input type="hidden" name="{$target}IdDel" value="{$targetId}">
-					<button class='{$deleteButtonClass}' type='submit' name='{$target}Button' value='delete'>Radera</button>
+					<input type="hidden" name="{$targetType}IdDel" value="{$targetId}">
+					<button class='{$deleteButtonClass}' type='submit' name='{$targetType}Button' value='delete'>Radera</button>
 				</form>
 			</div>
 		HERE;
