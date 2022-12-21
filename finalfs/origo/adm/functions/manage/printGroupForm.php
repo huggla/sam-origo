fe935643a039a36be6b60df3c696f23d1d874976
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-12-21 11:35:04 +0100

    Update printGroupForm.php

--- finalfs/origo/adm/functions/manage/printGroupForm.php
+++ finalfs/origo/adm/functions/manage/printGroupForm.php
@@ -1,33 +1,27 @@
 <?php
-	function printGroupForm($groupId)
+	function printGroupForm($group, $operationTables, $inheritPosts)
 	{
-		GLOBAL $hiddenInputs, $target;
-		echo <<<HERE
-		<div>
-			<div style="float:left;">
-				<form method="post" style="line-height:2">
-		HERE;
-		printTextarea($groupId, 'id', 'textareaMedium', 'Id:');
-		printTextarea($groupId, 'layers', 'textareaLarge', 'Lager:');
-		printTextarea($groupId, 'groups', 'textareaLarge', 'Grupper:');
-		echo 			'<br>';
-		printTextarea($groupId, 'title', 'textareaMedium', 'Titel:');
-		printUpdateSelect($groupId, 'expanded', 'miniSelect', 'Expanderad:', array("", "f", "t"));
-		printTextarea($groupId, 'abstract', 'textareaMedium', 'Sammanfattning:');
-		printTextarea($groupId, 'info', 'textareaLarge', 'Info:');
-		echo $hiddenInputs;
-		echo 			'<div class="buttonDiv">';
-		printUpdateButton();
-                printInfoButton($groupId);
-		echo 			'</div>';
-		echo 		'</form>';
-		echo 	'</div>';
-		$deleteConfirmStr="Är du säker på att du vill radera gruppen $groupId? Ingående lager påverkas ej och referenser till gruppen hanteras separat.";
-		printDeleteButton($groupId, $deleteConfirmStr, 'deleteButton2');
+		echo '<div><div style="float:left;"><form method="post" style="line-height:2">';
+		printTextarea($group, 'group_id', 'textareaMedium', 'Id:');
+		printTextarea($group, 'layers', 'textareaLarge', 'Lager:');
+		printTextarea($group, 'groups', 'textareaLarge', 'Grupper:');
+		echo '<br>';
+		printTextarea($group, 'title', 'textareaMedium', 'Titel:');
+		printUpdateSelect($group, array('expanded'=>array("f", "t")), 'miniSelect', 'Expanderad:');
+		printTextarea($group, 'abstract', 'textareaMedium', 'Sammanfattning:');
+		printTextarea($group, 'info', 'textareaLarge', 'Info:');
+		printHiddenInputs($inheritPosts);
+		echo '<div class="buttonDiv">';
+		printUpdateButton('group');
+		$group['group']=$group['group']['group_id'];
+		printInfoButton($group);
+		echo '</div></form></div>';
+		$deleteConfirmStr="Är du säker på att du vill radera gruppen ".$group['group']."? Ingående lager påverkas ej och referenser till gruppen hanteras separat.";
+		printDeleteButton($group, $deleteConfirmStr, 'deleteButton2');
 		echo '</div>';
-		printAddOperation('maps', $groupId, 'Lägg till i karta');
-		printRemoveOperation('maps', $groupId, 'Ta bort från karta');
-		printAddOperation('groups', $groupId, 'Lägg till i grupp');
-		printRemoveOperation('groups', $groupId, 'Ta bort från grupp');
+		printAddOperation($group, array('maps'=>array_column($operationTables['maps'], 'map_id')), 'Lägg till i karta', $inheritPosts);
+		printRemoveOperation($group, array('maps'=>$operationTables['maps']), 'Ta bort från karta', $inheritPosts);
+		printAddOperation($group, array('groups'=>array_column($operationTables['groups'], 'group_id')), 'Lägg till i grupp', $inheritPosts);
+		printRemoveOperation($group, array('groups'=>$operationTables['groups']),'Ta bort från grupp', $inheritPosts);
 	}
 ?>
