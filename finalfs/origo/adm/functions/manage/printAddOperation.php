ced9cab8732d76248501a5cb76ee544eea48f0d1
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-12-21 11:54:29 +0100

    Update printAddOperation.php

--- finalfs/origo/adm/functions/manage/printAddOperation.php
+++ finalfs/origo/adm/functions/manage/printAddOperation.php
@@ -1,13 +1,13 @@
 <?php
 	function printAddOperation($target, $addToTable, $buttontext, $inheritPosts)
 	{
-			$str=rtrim(ucfirst(key($addToTable)), 's');
-			echo '<form class="addForm" method="post">';
-			echo '<select class="headSelect" name="to'.$str.'Id">';
-			printSelectOptions(array_merge(array(""),current($addToTable)));
-			echo '</select>';
-			printHiddenInputs($inheritPosts);
-			echo '<button type="submit" name="'.key($target).'Button" value="operation">'.$buttontext.'</button>';
-			echo '</form>';
+		$str=rtrim(ucfirst(key($addToTable)), 's');
+		echo '<form class="addForm" method="post">';
+		echo '<select class="headSelect" name="to'.$str.'Id">';
+		printSelectOptions(array_merge(array(""),current($addToTable)));
+		echo '</select>';
+		printHiddenInputs($inheritPosts);
+		echo '<button type="submit" name="'.key($target).'Button" value="operation">'.$buttontext.'</button>';
+		echo '</form>';
 	}
 ?>
