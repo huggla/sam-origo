12920d17851f53b8a96de78b1bf75fc7bf5a98b2
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-12-21 11:46:28 +0100

    Update printTilegridForm.php

--- finalfs/origo/adm/functions/manage/printTilegridForm.php
+++ finalfs/origo/adm/functions/manage/printTilegridForm.php
@@ -1,25 +1,19 @@
 <?php
-	function printTilegridForm($targetId)
+	function printTilegridForm($tilegrid, $inheritPosts)
 	{
-		GLOBAL $hiddenInputs, $target;
-		echo <<<HERE
-		<div>
-			<div style="float:left;">
-				<form method="post" style="line-height:2">
-		HERE;
-		printTextarea($targetId, 'id', 'textareaMedium');
-		printTextarea($targetId, 'tilesize', 'textareaSmall', 'Tile-storlek:');
+		echo '<div><div style="float:left;"><form method="post" style="line-height:2">';
+		printTextarea($tilegrid, 'tilegrid_id', 'textareaMedium', 'Id:');
+		printTextarea($tilegrid, 'tilesize', 'textareaSmall', 'Tile-storlek:');
 		echo '</br>';
-		printTextarea($targetId, 'info', 'textareaLarge');
-		echo $hiddenInputs;
-		echo 			'<div class="buttonDiv">';
-		printUpdateButton();
-                printInfoButton($targetId);
-		echo 			'</div>';
-		echo 		'</form>';
-		echo 	'</div>';
-		$deleteConfirmStr="Är du säker att du vill radera tilegriden $targetId? Referenser till tilegriden hanteras separat.";
-		printDeleteButton($targetId, $deleteConfirmStr, 'deleteButton2');
+		printTextarea($tilegrid, 'info', 'textareaLarge', 'Info:');
+		printHiddenInputs($inheritPosts);
+		echo '<div class="buttonDiv">';
+		printUpdateButton('tilegrid');
+		$tilegrid['tilegrid']=$tilegrid['tilegrid']['tilegrid_id'];
+		printInfoButton($tilegrid);
+		echo '</div></form></div>';
+		$deleteConfirmStr="Är du säker att du vill radera tilegriden ".$tilegrid['tilegrid']."? Referenser till tilegriden hanteras separat.";
+		printDeleteButton($tilegrid, $deleteConfirmStr, 'deleteButton2');
 		echo '</div>';
 	}
 ?>
