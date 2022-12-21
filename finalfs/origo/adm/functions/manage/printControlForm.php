4308d526e9b8f63720da55559e05c70860691ff5
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-12-21 11:31:21 +0100

    Update printControlForm.php

--- finalfs/origo/adm/functions/manage/printControlForm.php
+++ finalfs/origo/adm/functions/manage/printControlForm.php
@@ -1,29 +1,22 @@
 <?php
-	function printControlForm($controlId)
+	function printControlForm($control, $maps, $inheritPosts)
 	{
-		GLOBAL $hiddenInputs, $target;
-		echo <<<HERE
-		<div>
-			<div style="float:left;">
-				<form method="post" style="line-height:2">
-		HERE;
-		printTextarea($controlId, 'id', 'textareaMedium');
-		printTextarea($controlId, 'options', 'textareaLarge', 'Inställningar:');
-		echo 			'<br>';
-		printTextarea($controlId, 'info', 'textareaLarge');
-		echo $hiddenInputs;
-		echo 			'<div class="buttonDiv">';
-		printUpdateButton();
-                printInfoButton($controlId);
-		echo 			'</div>';
-		echo 		'</form>';
-		echo 	'</div>';
-		$deleteConfirmStr="Är du säker att du vill radera kontrollen $controlId? Referenser till kontrollen hanteras separat.";
-		printDeleteButton($controlId, $deleteConfirmStr, 'deleteButton1');
-		echo '</div>';
-		echo '<div style="clear:both">';
-		printAddOperation('maps', $controlId, 'Lägg till i karta');
-		printRemoveOperation('maps', $controlId, 'Ta bort från karta');
+		echo '<div><div style="float:left;"><form method="post" style="line-height:2">';
+		printTextarea($control, 'control_id', 'textareaMedium', 'Id:');
+		printTextarea($control, 'options', 'textareaLarge', 'Inställningar:');
+		echo '<br>';
+		printTextarea($control, 'info', 'textareaLarge', 'Info:');
+		printHiddenInputs($inheritPosts);
+		echo '<div class="buttonDiv">';
+		printUpdateButton('control');
+		$control['control']=$control['control']['control_id'];
+        printInfoButton($control);
+		echo '</div></form></div>';
+		$deleteConfirmStr="Är du säker att du vill radera kontrollen ".$control['control']."? Referenser till kontrollen hanteras separat.";
+		printDeleteButton($control, $deleteConfirmStr, 'deleteButton1');
+		echo '</div><div style="clear:both">';
+		printAddOperation($control, array('maps'=>array_column($maps['maps'], 'map_id')), 'Lägg till i karta', $inheritPosts);
+		printRemoveOperation($control, $maps, 'Ta bort från karta', $inheritPosts);
 		echo '</div>';
 	}
 ?>
