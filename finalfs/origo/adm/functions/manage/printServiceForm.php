a6d8443b182472367f6d298c85138a689be5f7d1
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-12-21 11:43:36 +0100

    Update printServiceForm.php

--- finalfs/origo/adm/functions/manage/printServiceForm.php
+++ finalfs/origo/adm/functions/manage/printServiceForm.php
@@ -1,25 +1,19 @@
 <?php
-	function printServiceForm($targetId)
+	function printServiceForm($service, $inheritPosts)
 	{
-		GLOBAL $hiddenInputs, $target;
-		echo <<<HERE
-		<div>
-			<div style="float:left;">
-				<form method="post" style="line-height:2">
-		HERE;
-		printTextarea($targetId, 'id', 'textareaMedium');
-		printTextarea($targetId, 'base_url', 'textareaLarge', 'Huvudurl:');
+		echo '<div><div style="float:left;"><form method="post" style="line-height:2">';
+		printTextarea($service, 'service_id', 'textareaMedium', 'Id:');
+		printTextarea($service, 'base_url', 'textareaLarge', 'Huvudurl:');
 		echo '</br>';
-		printTextarea($targetId, 'info', 'textareaLarge');
-		echo $hiddenInputs;
-		echo 			'<div class="buttonDiv">';
-		printUpdateButton();
-                printInfoButton($targetId);
-		echo 			'</div>';
-		echo 		'</form>';
-		echo 	'</div>';
-		$deleteConfirmStr="Är du säker att du vill radera tjänsten $targetId? Referenser till tjänsten hanteras separat.";
-		printDeleteButton($targetId, $deleteConfirmStr, 'deleteButton2');
+		printTextarea($service, 'info', 'textareaLarge', 'Info:');
+		printHiddenInputs($inheritPosts);
+		echo '<div class="buttonDiv">';
+		printUpdateButton('service');
+		$service['service']=$service['service']['service_id'];
+		printInfoButton($service);
+		echo '</div></form></div>';
+		$deleteConfirmStr="Är du säker att du vill radera tjänsten ".$service['service']."? Referenser till tjänsten hanteras separat.";
+		printDeleteButton($service, $deleteConfirmStr, 'deleteButton2');
 		echo '</div>';
 	}
 ?>
