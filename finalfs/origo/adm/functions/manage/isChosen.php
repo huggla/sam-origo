fa8e46f252e42c4cf4f3112fc6aa371b6c05355d
Henrik Uggla <henrik.uggla@kristianstad.se>
2022-12-21 11:24:19 +0100

    Delete isChosen.php

--- finalfs/origo/adm/functions/manage/isChosen.php
+++
@@ -1,9 +0,0 @@
-<?php
-	function isChosen($id)
-	{
-		$target=$GLOBALS['target'];
-		$targetTable=$target.'s';
-		$targetTableArr=$GLOBALS[$targetTable];
-		return (isset($id) && in_array($id, array_column($targetTableArr, idColumnOfTarget($target))));
-	}
-?>
